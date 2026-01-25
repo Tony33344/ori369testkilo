9c33b030a30150b96511a8c5461e21a920141543
grenn99 <grenn99@users.noreply.github.com>
2026-01-25 13:45:32 +0100

    fix: add course base fields UI, update education subsite to light theme, add Slovenian translations

--- app/education/potrdi-rezervacijo/page.tsx
+++ app/education/potrdi-rezervacijo/page.tsx
@@ -155,7 +155,7 @@ export default function EducationReservationPage() {
                   </div>
                 ))}
               </div>
-            )
+            )}
           </div>
 
           {/* Action Button */}
